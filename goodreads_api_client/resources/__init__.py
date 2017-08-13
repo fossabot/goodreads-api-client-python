9acc29d51abbe6ce725dc50a0c1a2e4456070b80
Michelle D Zhang <zhang.michelle.d@gmail.com>
2017-08-13 11:25:20 -0400

    Add review methods

--- goodreads_api_client/resources/__init__.py
+++ goodreads_api_client/resources/__init__.py
@@ -10,6 +10,7 @@ the Goodreads API
 from goodreads_api_client.resources.author import Author
 from goodreads_api_client.resources.book import Book
 from goodreads_api_client.resources.comment import Comment
+from goodreads_api_client.resources.review import Review
 from goodreads_api_client.resources.series import Series
 from goodreads_api_client.resources.shelf import Shelf
 from goodreads_api_client.resources.topic import Topic
@@ -20,6 +21,7 @@ __all__ = [
     'Author',
     'Book',
     'Comment',
+    'Review',
     'Series',
     'Shelf',
     'Topic',
